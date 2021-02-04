f06054fccf8bc28d258cdeedf2e73ef754cdae05
atharvbakre <44296079+atharvbakre@users.noreply.github.com>
2021-02-04 12:53:31 +0530

    User Registration

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
-    return (
-        <footer class="bg-dark text-white mt-5 p-4 text-center">
-            Copyright &copy; 2021 DevHub
-        </footer>
-    )
+  return (
+    <footer className="bg-dark text-white mt-5 p-4 text-center">
+      Copyright &copy; 2021 DevHub
+    </footer>
+  );
 }
 
 export default Footer;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,105 +1,111 @@
-const express = require('express')
-const gravatar = require('gravatar')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const passport = require('passport')
-const keys = require('../../config/keys')
+const express = require("express");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+const keys = require("../../config/keys");
 
 // Validations
-const registerValidations = require('../../validation/register')
-const loginValidations = require('../../validation/login')
+const registerValidations = require("../../validation/register");
+const loginValidations = require("../../validation/login");
 
 // Load User model
-const User = require('../../models/User')
+const User = require("../../models/User");
 
-const router = express.Router()
+const router = express.Router();
 
 // @route   POST /api/users/register
 // @desc    Register new User
 // @access  Public
-router.post('/register', async (req, res) => {
-    const { errors, isValid } = registerValidations(req.body)
+router.post("/register", async (req, res) => {
+  const { errors, isValid } = registerValidations(req.body);
 
-    if(!isValid) {
-        return res.json(errors)
-    }
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
 
-    const user = await User.findOne({ email: req.body.email })
-    
-    if(user) {
-        errors.email = 'Email already exists!'
-        return res.status(400).json(errors)
-    }
+  const user = await User.findOne({ email: req.body.email });
 
-    const avatar = gravatar.url(req.body.email, {
-        s: '200',
-        r: 'pg',   
-        d: 'mm'
-    })
+  if (user) {
+    errors.email = "Email already exists!";
+    return res.status(400).json(errors);
+  }
 
-    const newUser = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-        avatar,
-        date: req.body.date
-    })
+  const avatar = gravatar.url(req.body.email, {
+    s: "200",
+    r: "pg",
+    d: "mm",
+  });
 
-    newUser.password = await bcrypt.hash(newUser.password, 8)
-    await newUser.save()
-    res.json(newUser)
-})
+  const newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    avatar,
+    date: req.body.date,
+  });
+
+  newUser.password = await bcrypt.hash(newUser.password, 8);
+  await newUser.save();
+  res.json(newUser);
+});
 
 // @route   POST /api/users/login
 // @desc    Login and get JWT Token
 // @access  Public
-router.post('/login', async (req, res) => {
-    const { errors, isValid } = loginValidations(req.body)
+router.post("/login", async (req, res) => {
+  const { errors, isValid } = loginValidations(req.body);
 
-    if(!isValid) {
-        return res.json(errors)
-    }
+  if (!isValid) {
+    return res.json(errors);
+  }
 
-    const email = req.body.email
-    const password = req.body.password
+  const email = req.body.email;
+  const password = req.body.password;
 
-    const user = await User.findOne({ email })
+  const user = await User.findOne({ email });
 
-    if(!user) {
-        errors.email = 'Invalid Email'
-        return res.status(404).json(errors)
-    }
+  if (!user) {
+    errors.email = "Invalid Email";
+    return res.status(404).json(errors);
+  }
 
-    if(await bcrypt.compare(password, user.password)) {
-        // User found
-        const payload = {
-            id: user.id,
-            name: user.name,
-            avatar: user.avatar
-        }
-        
-        // JWT Token
-        const token = await jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 })
-        return res.json({ 
-            sucess: true, 
-            token: 'Bearer ' + token 
-        })
-    }
+  if (await bcrypt.compare(password, user.password)) {
+    // User found
+    const payload = {
+      id: user.id,
+      name: user.name,
+      avatar: user.avatar,
+    };
 
-    // Incorrect password
-    errors.password = 'Incorrect Password' 
-    res.status(400).json(errors) 
-})
+    // JWT Token
+    const token = await jwt.sign(payload, keys.secretOrKey, {
+      expiresIn: 3600,
+    });
+    return res.json({
+      sucess: true,
+      token: "Bearer " + token,
+    });
+  }
+
+  // Incorrect password
+  errors.password = "Incorrect Password";
+  res.status(400).json(errors);
+});
 
 // @route   GET /api/users/current
 // @desc    Current user
 // @access  Private
-router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
     res.json({
-        id: req.user.id, 
-        name: req.user.name,
-        email: req.user.email
-    })
-})
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email,
+    });
+  }
+);
 
-module.exports = router
+module.exports = router;
